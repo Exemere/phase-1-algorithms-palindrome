f4f8b1ff36c75cb594975dd407de543d83926f5a
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-26 18:37:38 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+ 
+  // Compare the original string with its reverse
+  return str === str.split('').reverse().join('');
 }
+
+console.log(isPalindrome("mom"));      
+console.log(isPalindrome("racecar")); 
+console.log(isPalindrome("hello"));   
+console.log(isPalindrome("Able was I ere I saw Elba"));
 
 /* 
   Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
-*/
+  displays if one is a grown up or child based on age */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
